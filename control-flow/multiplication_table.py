ca7c97456c18ae06796a20ea2b8ad0ad8bd62b76
saadelbonix <saad.elboni2@gmail.com>
2025-05-31 22:22:41 +0100

    Update multiplication_table.py

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,15 +1,3 @@
-"""Enter a number to see its multiplication table: 5
-5 * 1 = 5
-5 * 2 = 10
-5 * 3 = 15
-5 * 4 = 20
-5 * 5 = 25
-5 * 6 = 30
-5 * 7 = 35
-5 * 8 = 40
-5 * 9 = 45
-5 * 10 = 50"""
-
 num = int(input("Enter a number to see its multiplication table:"))
 for e in range(1,11):
     print(f"{num} * {e}","=",num * e)
