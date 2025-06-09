5a93f9545320e814d69ad796298eaac1859c3287
saadelbonix <saad.elboni2@gmail.com>
2025-06-09 16:16:59 +0100

    Update test_simple_calculator.py

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -7,9 +7,9 @@ class TestSimpleCalculator(unittest.TestCase):
         self.calc = SimpleCalculator()
 
     def test_addition(self):
-        self.assertEqual(self.calc.add(1, 2), 3)
-        self.assertEqual(self.calc.add(-1, -2), -3)
+        self.assertEqual(self.calc.add(2, 3), 5)
         self.assertEqual(self.calc.add(-1, 1), 0)
+        self.assertEqual(self.calc.add(0, 0), 0)
 
     def test_subtraction(self):
         self.assertEqual(self.calc.subtract(5, 3), 2)
@@ -18,13 +18,14 @@ class TestSimpleCalculator(unittest.TestCase):
 
     def test_multiply(self):
         self.assertEqual(self.calc.multiply(2, 3), 6)
-        self.assertEqual(self.calc.multiply(-2, 3), -6)
-        self.assertEqual(self.calc.multiply(0, 3), 0)
+        self.assertEqual(self.calc.multiply(-1, 4), -4)
+        self.assertEqual(self.calc.multiply(0, 100), 0)
 
     def test_divide(self):
         self.assertEqual(self.calc.divide(10, 2), 5)
         self.assertEqual(self.calc.divide(9, 3), 3)
-        self.assertIsNone(self.calc.divide(5, 0))  # Division by zero
+        self.assertEqual(self.calc.divide(-6, 2), -3)
+        self.assertIsNone(self.calc.divide(5, 0))  # Check for division by zero
 
 if __name__ == '__main__':
     unittest.main()
