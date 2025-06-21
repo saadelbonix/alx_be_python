2b00046a2f214f5db544d6e9f4f84fb1c2d58fd6
saadelbonix <saad.elboni2@gmail.com>
2025-06-21 12:48:59 +0100

    Update library_system.py

--- oop/library_system.py
+++ oop/library_system.py
@@ -1,35 +1,34 @@
 class Book:
-    def __init__(self, title, author, year):
+    def __init__(self, title, author):
         self.title = title
         self.author = author
-        self.year = year
 
     def get_details(self):
-        return f"{self.title} by {self.author}, published in {self.year}"
+        return f"Book: {self.title} by {self.author}"
 
     def __str__(self):
         return self.get_details()
 
 
 class EBook(Book):
-    def __init__(self, title, author, year, file_size):
-        super().__init__(title, author, year)
+    def __init__(self, title, author, file_size):
+        super().__init__(title, author)
         self.file_size = file_size
 
     def get_details(self):
-        return f"EBook: {self.title} by {self.author}, published in {self.year}, File Size: {self.file_size}KB"
+        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
 
     def __str__(self):
         return self.get_details()
 
 
 class PrintBook(Book):
-    def __init__(self, title, author, year, page_count):
-        super().__init__(title, author, year)
+    def __init__(self, title, author, page_count):
+        super().__init__(title, author)
         self.page_count = page_count
 
     def get_details(self):
-        return f"PrintBook: {self.title} by {self.author}, published in {self.year}, Page Count: {self.page_count}"
+        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
 
     def __str__(self):
         return self.get_details()
